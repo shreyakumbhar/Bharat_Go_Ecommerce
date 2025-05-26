3489f7a8f1d2766ec04ad78b4cd9160e9dbf466c
Shreya Kumbhar <shreyakumbhar16327@gmail.com>
2025-05-26 18:32:32 +0530

    Update client folder with latest changes

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,7 @@ const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
 
-    // host: process.env.DB_HOST,
-    // user: process.env.DB_USER,
-    // database: process.env.DB_NAME,
-    // password: process.env.DB_PASS,
-    // port: process.env.DB_PORT,
-    // connectionLimit: 10,
+ 
 
         host: "localhost",
     user: "root",
